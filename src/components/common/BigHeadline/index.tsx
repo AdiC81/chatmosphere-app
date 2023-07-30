763237e08ae24ff9f4c3339bad969eb54e200fdf
Adrian Contiu <adrian.contiu@toptal.com>
2023-07-30 15:53:19 +0300

    Fourth commit

--- src/components/common/BigHeadline/index.tsx
+++ src/components/common/BigHeadline/index.tsx
@@ -4,6 +4,6 @@ import styled from "styled-components";
 export const BigHeadline = styled.h1`
 font-size: 2.5rem;
 font-weight: 500;
-color: ${(props) => props.theme.text.warning};
+color: ${(props) => props.theme.text.primary};
 margin: 0;
 `
